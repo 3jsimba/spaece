1dedde0a0f2eb3cb23f815d15f3127b60731a410
Emmanuel Jerez <75187415+3jsimba@users.noreply.github.com>
2024-02-06 16:52:13 -0800

    fixed space issue

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import './fonts/public-sans-v2/fonts/ttf/PublicSans-Black.ttf'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -12,4 +11,4 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
+// reportWebVitals();
